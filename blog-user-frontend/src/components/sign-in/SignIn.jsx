737193da1e40f533b4bacf2a5a251f3218588a01
G-Alessandro <galloalessandro77@gmail.com>
2024-05-21 19:23:23 +0200

    Added style to SignIn and SignUp

--- blog-user-frontend/src/components/sign-in/SignIn.jsx
+++ blog-user-frontend/src/components/sign-in/SignIn.jsx
@@ -36,31 +36,33 @@ export default function SignIn() {
   };
 
   return (
-    <div className={style.container}>
+    <>
       <TopBar />
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          minLength={1}
-          maxLength={30}
-          required
-        />
+      <div className={style.signInFormContainer}>
+        <form onSubmit={handleSubmit} className={style.signInForm}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            minLength={1}
+            maxLength={30}
+            required
+          />
 
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          minLength={8}
-          maxLength={16}
-          required
-        />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            minLength={8}
+            maxLength={16}
+            required
+          />
 
-        <button type="submit">Done</button>
-      </form>
-    </div>
+          <button type="submit">Done</button>
+        </form>
+      </div>
+    </>
   );
 }
--- blog-user-frontend/src/components/sign-up/SignUp.jsx
+++ blog-user-frontend/src/components/sign-up/SignUp.jsx
@@ -3,42 +3,43 @@ import style from "./SignUp.module.css";
 
 export default function SignUp() {
   return (
-    <div className={style.container}>
+    <>
       <TopBar />
-      <form action="http://localhost:3000/sign-up" method="post">
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          minLength={1}
-          maxLength={30}
-          required
-        />
+      <div className={style.signUpFormContainer}>
+        <form action="http://localhost:3000/sign-up" method="post" className={style.signUpForm}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            minLength={1}
+            maxLength={30}
+            required
+          />
 
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          minLength={8}
-          maxLength={16}
-          required
-        />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            minLength={8}
+            maxLength={16}
+            required
+          />
 
-        <label htmlFor="confirm-password">Confirm Password</label>
-        <input
-          type="password"
-          id="confirm-password"
-          name="confirm-password"
-          minLength={8}
-          maxLength={16}
-          required
-        />
+          <label htmlFor="confirm-password">Confirm Password</label>
+          <input
+            type="password"
+            id="confirm-password"
+            name="confirm-password"
+            minLength={8}
+            maxLength={16}
+            required
+          />
 
-        <button type="submit">Done</button>
-
-      </form>
-    </div>
+          <button type="submit">Done</button>
+        </form>
+      </div>
+    </>
   );
 }
